59b8c0c4648e6c16f328b54f35b5b5508e2483cb
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2025-06-04 16:50:23 -0300

    Implemented LocalStorage in agendamento.js: Adds persistent storage for scheduling data

--- src/stores/agendamento.js
+++ src/stores/agendamento.js
@@ -1,8 +1,15 @@
 import { defineStore } from 'pinia'
+import { LocalStorage } from 'quasar'
+
 
 export const useAgendamentoStore = defineStore('agendamento', {
+  // Define o estado inicial do store
   state: () => ({
-    agendamentos: [],
+
+    // Busca no LocalStorage por agendamentos salvos ou inicia com um array vazio
+    agendamentos: LocalStorage.getItem('agendamentos') || [],
+
+    // Define os horários disponíveis para agendamento
     horarios: {
       manha: { start: 8, end: 10, price: 120 },
       tarde: { start: 15, end: 17, price: 160 },
@@ -10,22 +17,27 @@ export const useAgendamentoStore = defineStore('agendamento', {
     }
   }),
 
-  // Getters
   actions: {
+    // Método para adicionar um agendamento
     addAgendamento(agendamento) {
       this.agendamentos.push(agendamento)
+      this.saveToLocalStorage()
     },
+    // Método para atualizar um agendamento
     removeAgendamento(agendamentoId) {
       this.agendamentos = this.agendamentos.filter(a => a.id !== agendamentoId)
+      this.saveToLocalStorage()
     },
-    
+    // Método para buscar agendamentos por data
     getAgendamentosData(date) {
       return this.agendamentos.filter(agendamento => agendamento.date === date)
     },
+    // Método para buscar agendamentos por período
     getHorariosDisponiveis(date) {
       const agendados = this.getAgendamentosData(date)
       const todos = []
       
+      // Itera sobre os períodos definidos e cria uma lista de horários disponíveis
       for (const [period, details] of Object.entries(this.horarios)) {
         for (let hour = details.start; hour < details.end; hour++) {
           todos.push({
@@ -38,6 +50,11 @@ export const useAgendamentoStore = defineStore('agendamento', {
       }
       
       return todos
+    },
+    
+    saveToLocalStorage() {
+      // Salva os agendamentos no LocalStorage
+      LocalStorage.set('agendamentos', this.agendamentos)
     }
   }
 })

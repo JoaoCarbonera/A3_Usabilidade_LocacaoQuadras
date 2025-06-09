97084913d70456075d764e3ec8f982dd1d081ee0
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2025-06-09 17:56:00 -0300

    commit: Refactors login form and adds mock API script

---
+++ src/stores/usuario.js
@@ -0,0 +1,53 @@
+import { defineStore } from 'pinia'
+import { LocalStorage } from 'quasar'
+import axios from 'axios'
+
+export const useUsuarioStore = defineStore('user', {
+    state: () => ({
+        // Busca no LocalStorage por usuário logado ou inicia com null
+        user: LocalStorage.getItem('user') || null
+    }),
+
+    getters: {
+        // Verifica se o usuário está logado
+        isLogado: (state) => !!state.user,
+
+        // Retorna o nome do usuário ou 'Visitante' se não estiver logado
+        userName: (state) => (state.user ? state.user.name : 'Visitante')
+    },
+
+    actions: {
+
+        // Método async para salvar o usuário no LocalStorage
+        async login(credenciais) {
+            try {
+
+                // Faz uma requisição GET para o endpoint de login com as credenciais
+                const response = await axios.get(`http://localhost:3001/username${credenciais.username}&password=${credenciais.password}`)
+
+                if (response.data.length > 0) {
+
+                    // Se a resposta contiver dados, extrai o usuário e salva no estado e no LocalStorage
+                    const user = response.data[0]
+                    this.user = { id: user.id, name: user.name, username: user.username }
+                    localStorage.set('user', this.user)
+                    return true
+                }
+                // Se não houver dados, retorna false
+                return false
+
+            } catch (error) {
+                // Em caso de erro, exibe uma mensagem no console e retorna false
+                console.error('Erro ao fazer login:', error)
+                return false
+            }
+        },
+
+        logout() {
+
+            // Método para limpar o usuário do estado e do LocalStorage
+            this.user = null
+            localStorage.remove('user')
+        }
+    }
+})
